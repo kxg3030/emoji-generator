28c17cf8648b4e76b30449e39d5c5e08acf03d02
zhangtao <zhangtao@xinchao.mobi>
2019-03-27 14:05:37 +0800

    延长jwt过期时间

--- Index.go
+++ Index.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"emoji/pkg/bootstrap"
-	"emoji/pkg/unity"
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main()  {
-	fmt.Print(unity.GetToken("openId","a"))
 	engine := bootstrap.NewBootstrap(gin.New()).Init()
 	engine.Run(":8080")
 }
