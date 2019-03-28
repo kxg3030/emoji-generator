b80a320c0bd9d5cfc73f33fe06d622c164ebcc09
zhangtao <zhangtao@xinchao.mobi>
2019-03-28 13:52:47 +0800

    update

--- pkg/config/Config.go
+++ pkg/config/Config.go
@@ -27,7 +27,7 @@ var Extension = []string{
 
 var Config = map[string]interface{}{
 	"ListenPort"       : "0.0.0.0:9527",
-	"DebugMode"        : true,
+	"DebugMode"        : false,
 	"GlobalMiddleWare" : []middleware.MiddlewareInterface{
 		middleware.NewCrossSiteMiddleware(),
 		middleware.NewRecoverMiddleware(),
